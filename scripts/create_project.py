fedc7e215821c14dc11f6a5c928ed30e01162a64
Daniel Williams <dwilliams@port8080.net>
2026-08-27 10:53:14 -0600

    Using the wrong environs.

--- scripts/create_project.py
+++ scripts/create_project.py
@@ -17,10 +17,10 @@ from api_helpers.projects import create_project
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action = "store_true")
-    parser.add_argument("--token", default = os.getenv("ARTIFACTORY_TOKEN", ""),
-                        help = "Artifactory access token to use for requests.  Will use ARTIFACTORY_TOKEN if not specified.")
-    parser.add_argument("--host", default = os.getenv("ARTIFACTORY_HOST", ""),
-                        help = "Artifactory host URL (e.g. https://artifactory.example.com/) to use for requests.  Will use ARTIFACTORY_HOST if not specified.")
+    parser.add_argument("--token", default = os.getenv("JFROG_ADMIN_TOKEN", ""),
+                        help = "Artifactory access token to use for requests.  Will use JFROG_ADMIN_TOKEN if not specified.")
+    parser.add_argument("--host", default = os.getenv("JFROG_URL", ""),
+                        help = "Artifactory host URL (e.g. https://artifactory.example.com/) to use for requests.  Will use JFROG_URL if not specified.")
 
     parser.add_argument("project_key", help = "Short version of the project name used for identifying the project.")
     parser.add_argument("project_name", help = "Full name of the project.")
